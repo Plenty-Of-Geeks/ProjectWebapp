be44beaa95f11b12e882972da2f36ce25617de10
Yu-Hsin Sean Wang <katnapped@hotmail.com>
2015-01-28 16:14:27 -0800

    Added Template.  Please let me know what you think.

---
+++ application/views/_menubar.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+
+<ul>
+    <li class="start selected"><a href="index.html">Home</a></li>
+    <li class=""><a href="examples.html">Examples</a></li>
+    <li><a href="#">Products</a></li>
+    <li><a href="#">Solutions</a></li>
+    <li class="end"><a href="#">Contact</a></li>
+</ul>
