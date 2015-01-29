9f4710fdd7296b69df16bc295831e5d59f5a6c07
Yu-Hsin Sean Wang <katnapped@hotmail.com>
2015-01-29 10:12:38 -0800

    Updated Menubar. Moved body content to views.

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -9,9 +9,9 @@
 ?>
 
 <ul>
-    <li class="start selected"><a href="index.html">Home</a></li>
-    <li class=""><a href="examples.html">Examples</a></li>
+    <li class="start selected"><a href="/Welcome">Home</a></li>
+    <li class=""><a href="examples.html">Posts</a></li>
     <li><a href="#">Products</a></li>
-    <li><a href="#">Solutions</a></li>
+    <li><a href="#">About</a></li>
     <li class="end"><a href="#">Contact</a></li>
 </ul>
