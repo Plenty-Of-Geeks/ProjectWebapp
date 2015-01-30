e8f2e3d834f86b648182feb52733fbc029a4c208
thunduaga <chriswumbojumbo@msn.com>
2015-01-29 16:53:21 -0800

    4:53pm 29/01/2015 Signed-off-by:thunduaga <chriswumbojumbo@msn.com>

---
+++ application/views/about.php
@@ -0,0 +1,70 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<section id="content">
+    <article>
+        <h2>About Plenty of Geeks</h2>
+             
+        <p><strong>Plenty of Geeks is a medium used to connect Game Designers and Game Programmers</strong></p>
+    
+    <p>Often many Game Designers have great video game ideas that go un-pursued 
+        due to the lack of Programmer resources. The resources available are typically 
+        unsuited for small groups or organizations who are looking for not just a generic
+        code monkey but rather inspired individuals who share a passion for Game Design.</p>
+        
+        <p>Plenty of Geeks allows users to post a game project via our Posts page.
+        Here any user may view this project and leave comments, share thoughts
+        and offer their support towards the project. </p>
+        
+        <p>Users wanting to post a Game Project idea have the option of creating a
+        "Party Quest" posting. The poster, or leader, will be able to allocate a
+        specified amount of slots to allow for other users to join the project.
+        The leader can pick from the available users who show interest in being 
+        apart of the development to be apart of the project. As users are added
+        to this project team or "Party" the slots fill up, allowing for other users
+        to be aware of how many other users are helping this project become a reality.</p>
+        
+        <p>This way users can aquire a team of ideal minds for the creation of a 
+        video game quickly and directly. </p>
+        
+        <p>Additionally, users have the option of posting a game idea for the purpose
+        of publishing the idea to the public. This way, if a user has no knowledge
+        of video game programming but wishes to see a video game created, they can
+        expose their idea to those with the power to bring that game to life.</p>
+        
+        <p>All posts have a vote element, up vote and down vote. To insure the quality
+        of the ideas and projects released here, every post has a threshold to which
+        it can be down voted, once this threshold is reached the post is deleted.</p>
+        
+        <h3>About our users </h3>
+        
+        <p>Each user profile contains that users "Job Class"
+        this simply illustrates whether they are purely a game designer,
+        a game programmer, or both. Game designers may or may not have any 
+        previous programming background, these users are seen as to provide
+        the ideas behind a video game while the game programmers are seen to
+        construct the underlying foundation for the game to be functional.
+        </p>
+        
+        <p>Users who will both be conjuring video game ideas and programming
+        the implementation of game Project ideas fall under both categories
+        Game Designer and Game Programmer.</p>
+        
+        <p><strong>Everyone is Welcome </strong> </p>
+        <p>Game Designers, Game Programmers, storyboard writers, pixel artists,
+        3D modelers, animators, even if you know nothing about computer programming
+        and all you want to do is share a great video game idea</p>
+        <p><strong>This is the place for you </strong></p>
+        
+        <p>Each user profile contains a small list of their  </p>
+        
+        
+   	
+    </article>      
+</section>
