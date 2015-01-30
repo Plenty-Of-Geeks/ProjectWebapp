51e03ce828955deddc16992676b525f230710e69
LuckiiGoggy <luck-13@live.ca>
2015-01-29 16:16:06 -0800

    Added Contact A post page a mock post/quest Signed-off-by:LuckiiGoggy <luck-13@live.ca>

---
+++ application/views/mockquest.php
@@ -0,0 +1,48 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+<style>
+    .wheatBox{
+        box-shadow: 5px 5px 5px 5px;
+        border-radius: 5px;
+        padding: 2px;
+        background: wheat;
+    }
+    
+    
+    #title{
+        text-align: center;
+    }
+    
+    #quest{
+        
+    }
+</style>
+
+
+
+
+<section id="content">
+    <div id="title" class="wheatBox">
+        <h1>This is a mock quest</h1>
+        <h3>Quest Giver: {poster}</h3>
+    </div>
+    
+    <div id="quest" class="wheatBox">
+        
+        <div>
+            <h3>Details:</h3>
+
+        </div>    
+    </div>  
+</section>
+
+
+
