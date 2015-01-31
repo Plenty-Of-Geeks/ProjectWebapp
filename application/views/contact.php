0f0d75aa9889ef8225dc0034ee97d3a0ef9dfaf3
Edward <ed.leung14@gmail.com>
2015-01-30 22:01:07 -0800

    Updated Contact page with contact form

--- application/views/contact.php
+++ application/views/contact.php
@@ -7,19 +7,51 @@
  */
 ?>
 
+<script>
+$(document).ready(function(){
+    $("result").hide();
+    
+    $("#submit").click(function(){
+        $("result").show();
+    });
+});
+</script>
+
+<!-- The jquery above was suppose to show/hide the resulting form info when 
+    a user hit submit. BUt i forgot there's not php in the view...
+-->
 
 
+<div id="page" style="margin-left: 25%; margin-right: 25%; margin-top: 1em; width: 100%;">
 
-<section id="content">
-    <article style="text-align: center">
-        <h2>The Creators</h2>
-             
-        <h3>Chris Qualizza</h3>
-        <h3>Edward Leung</h3>
-        <h3>Yu-Hsin Sean Wang</h3>
-        <h3>George Padua</h3>
+    <h2>Contact</h2>					
 
-    </article>      
+    <p>If you wish to contact us, please fill out the form below and we will get back
+        to you as soon as possible.
+    </p>
+    <form method="post" action="">
+    <input type="hidden" name="recipient" value="something@somewhere.com">
+    <strong>
+        Name:<br>
+        <input type="text" name="name" id="name" value="John Doe"><br><br>
+
+        E-mail:<br>
+        <input type="text" name="email" value="Johndoe@example.com"><br><br>
+
+        Comment:<br>
+        <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+
+        <br>
+        <br>
+
+        <input type="submit" value="Submit" id="submit">
+        <input type="reset" value="Reset"><br>
+    </strong>
+    </form>
+
+</div>
+
+
 </section>
 
 
